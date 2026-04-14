fb7632f2213c77f519721db927098cea7144822b
ProgrammerRoy <royanwesa15official@gmail.com>
2026-04-14 19:37:46 +0530

    End of Section 7

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { Home } from '../features/home/home';
-
 import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
 import { Lists } from '../features/lists/lists';
 import { Messages } from '../features/messages/messages';
 import { MemberList } from '../features/members/member-list/member-list';
 import { authGuard } from '../core/guards/auth-guard';
+import { TestErrors } from '../features/test-errors/test-errors';
+import { NotFound } from '../shared/errors/not-found/not-found';
+import { ServerError } from '../shared/errors/server-error/server-error';
+
+
 
 export const routes: Routes = [
     {path: '', component: Home},
@@ -19,6 +23,8 @@ export const routes: Routes = [
             {path: 'lists', component: Lists},
             {path: 'messages', component: Messages},
         ]
-    },    
-    {path: '**', component: Home},
+    },  
+    {path: 'errors', component: TestErrors},  
+    {path: 'serve-error', component: ServerError},
+    {path: '**', component: NotFound},
 ];
